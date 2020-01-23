9ce1ebaa992b072dfe7f3eb52f3ee731d5842ad0
riveeenxu <rivenzyx@gmail.com>
2020-01-23 17:29:32 -0600

    Allows file upload, post and send to PyYAML

--- yamlyzer/yamlview/views.py
+++ yamlyzer/yamlview/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import yaml
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 # Create your views here.
-def index(request):
-    context = { }
-    return render(request, "yamlview/index.html", context)
+# def index(request):
+#     context = { }
+#     return render(request, "yamlview/index.html", context)
+
+
+def upload_file(request):
+    logger.info("A request is made")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        #fs = FileSystemStorage()
+        logger.info(myfile.name)
+        #filename = fs.save(myfile.name, myfile)
+        #uploaded_file_url = fs.url(filename)
+        readyforparse = yaml.load_all(myfile)
+        # send to parser here
+
+        # return, render the generated page based on dictionary form and filtered by the parser
+
+        return render(request, 'yamlview/index.html', {
+            'uploaded_file_url': myfile.name
+         })
+    return render(request, 'yamlview/index.html', { })
--- yamlyzer/yamlyzer/settings.py
+++ yamlyzer/yamlyzer/settings.py
@@ -119,3 +119,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# Logger
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'NOTSET',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'NOTSET',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'ERROR'
+        }
+    }
+}
