791f6d1ada8338a959a00ab426d3bed4b054d8b6
Kyle Fricilone <kfricilone@gmail.com>
2020-03-30 18:00:03 -0500

    Add error page
    Co-authored-by: riveeenxu <rivenzyx@gmail.com>

--- yamlyzer/yamlview/views.py
+++ yamlyzer/yamlview/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import Http404
 import yaml
 # import the logging library
 import logging
@@ -19,11 +20,12 @@ def index(request):
         logger.info(myfile.name)
         #filename = fs.save(myfile.name, myfile)
         #uploaded_file_url = fs.url(filename)
-        parser = Parse(myfile.name, yaml.load_all(myfile, Loader = yaml.FullLoader))
-        # send to parser here
-
-        # return, render the generated page based on dictionary form and filtered by the parser
-
-        return render(request, 'yamlview/index.html', parser.getData())
+        try:
+            # create parser
+            parser = Parse(myfile.name, yaml.load_all(myfile, Loader = yaml.FullLoader))
+            # get parsed data, send to template
+            return render(request, 'yamlview/index.html', parser.getData())
+        except:
+            return render(request, 'yamlview/error.html')
 
     return redirect('/')
